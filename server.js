5ca5ee76cf413a9a0b9af7f973d7d9a6ae769e64
endokagi <38246228+endokagi@users.noreply.github.com>
2019-12-03 04:26:04 +0700

    apply to client

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 $(function () {
 
-    for (var i = 0; i < 6; i++) {
-        // var rec = `<div class="carousel-item active" modifier="nodivider">
-        //     <img src="img/honkai icon.png" alt="">
-        //     ITEM
-        // </div>`;
+    $.get("http://localhost:8080/googleapp", function (data,status) {
+        console.log("topten");
+        data.forEach(element => {
+            var topten = `<div class="col-sm-2">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">${element.app}</h5>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>`;    
 
-        var rec = `<div class="col">
-        <img src="img/honkai icon.png">Item ${i}
-        </div>`;
-        $("#recommend").append(rec);
-    }
+        $("#test").append(topten);
+        });
+        
+    });
 
 });
